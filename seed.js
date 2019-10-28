87969559a35ee6765cbaa4a6792d38bf4ff5be43
Benjamin Torres <46063446+YoungPadObi@users.noreply.github.com>
2019-10-28 12:49:08 -0400

    Delete seed.js

--- seed.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const DATA = [
-  { name: 'Apples', checked: false },
-  { name: 'Oranges', checked: true },
-  { name: 'Carrots', checked: false },
-  { name: 'Mango', checked: false }
-];
