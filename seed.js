e50593760b09c2932f0b680447448bcdbd54f460
Ben Torres <Torres.Ben@gmail.com>
2019-10-28 12:45:58 -0400

    Initial commit

---
+++ seed.js
@@ -0,0 +1,7 @@
+'use strict';
+const DATA = [
+  { name: 'Apples', checked: false },
+  { name: 'Oranges', checked: true },
+  { name: 'Carrots', checked: false },
+  { name: 'Mango', checked: false }
+];
